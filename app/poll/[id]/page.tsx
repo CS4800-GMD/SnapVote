aba114ff7e1f2bcc2bfc3c1d677825b064b1cb1c
Husain Kahil <nyrikipvme@gmail.com>
2025-06-21 18:32:52 -0700

    surely this is the last error fix

--- app/poll/[id]/page.tsx
+++ app/poll/[id]/page.tsx
@@ -1,5 +1,5 @@
 import PollClient from './PollClient'
 
-export default function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
   return <PollClient id={params.id} />
 }
